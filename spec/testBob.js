745da4c6fff47d1b1876d522679ae5bfb1b519cf
Pranay Pradhananga <ppra36@student.monash.edu>
2017-06-22 17:25:43 +1000

    Added new test spec

---
+++ spec/testBob.js
@@ -0,0 +1,11 @@
+
+
+describe("Test Bob the Builder", function() {
+  describe("Bob sends the message", function() {
+    it("returns the correct roomId", function(done) {
+      
+    })
+
+  })
+
+})
